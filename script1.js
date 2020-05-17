8c7795b1c014bb36a091fe538e182f1296555f62
zul'jin <53787083+6mahdapihka9@users.noreply.github.com>
2020-05-17 19:43:37 +0300

    scripts began to act as they should

--- script1.js
+++ script1.js
@@ -1,8 +1,11 @@
-let __main = document.getElementById('__main');
-let __aside = document.getElementById('__aside');
-let __wall = document.getElementById('__wall');
-let MSI2020 = document.getElementById('MSI2020');
-let varient;
+const __main = document.getElementById('__main');
+const __aside = document.getElementById('__aside');
+const __wall = document.getElementById('__wall');
+const MSI2020 = document.getElementById('MSI2020');
+const fav_container = document.getElementById('fav-container');
+const jokes_container = document.getElementById('jokes-container');
+let variant = 'varRandom';
+
 if (window.innerWidth > 1239)
     __main.style.width = "" + (+window.innerWidth - 520) + "px";
 
@@ -35,6 +38,7 @@ function changeMenuVar(text) {
     textArea.hidden = true;
     switch (text) {
         case 'Random':
+            variant = 'varRandom';
             document.getElementById('circle-outer-random').style.border = "2px solid black";
             document.getElementById('circle-inner-random').style.backgroundColor ="black";
             break;
@@ -50,6 +54,7 @@ function changeMenuVar(text) {
             break;
     }
 }
+
 function chooseVar(text) {
     let varAnimal = document.getElementById('varAnimal');
     let varCareer = document.getElementById('varCareer');
@@ -68,4 +73,128 @@ function chooseVar(text) {
 
     document.getElementById(''+text).style.border = '2px solid #000';
     document.getElementById(''+text).style.color = '#000';
+    variant = text;
 }
+
+document.images.onclick = ()=>{
+    console.log("dsccsd");
+    favor(this.id);
+};
+/*
+document.getElementsByClassName('outer-jokes-div').onclick = () =>{
+    console.log("dsccsd"+this.children[0].id);
+};
+
+ */
+
+function favor(id) {
+
+    let divs = document.getElementsByClassName('favorites');
+    for (let i = divs.length - 1; i >= 0; i--)
+        divs[i].remove();
+
+    let favorites = [];
+    let __cookiesNameAndValue;
+    let updateCookies;
+    if (document.cookie !== '' || document.cookie !== "undefined") {
+        let __cookies = document.cookie.split('; ');
+        for (let index = 0; index < __cookies.length; index++) {
+            __cookiesNameAndValue = __cookies[index].split('=');
+            if (__cookiesNameAndValue[0] === 'IDS') {
+                favorites = __cookiesNameAndValue[1].split(',');
+                break;
+            }
+        }
+    }
+    let addToFav;
+    if (document.cookie.indexOf( id ) !== -1) {
+        addToFav = "";
+        favorites.unshift(id);
+    } else
+        addToFav = id + ',';
+
+    updateCookies = 'IDS=' + addToFav + ((__cookiesNameAndValue[1]!==undefined)?__cookiesNameAndValue[1]:'') + '; path=/; max-age=10';
+
+
+    deleteAllCookies();
+    document.cookie = updateCookies;
+    divs = [];
+    if (favorites !== undefined )
+        for (let i = 0; i < favorites.length; i++)
+            if (favorites[i] !== '' && favorites[i] !== undefined) {
+                let div;
+                div = document.createElement('div');
+                div.id = favorites[i] + "_fav";
+                div.className = "favorites";
+                let commentImg = document.createElement('img');
+                commentImg.src = "commentGrey.png";
+                commentImg.style.width = "40px";
+
+                let heartImg = document.createElement('img');
+                heartImg.src = "heartFullWhite.png";
+                heartImg.style.width = "20px";
+
+                let p = document.createElement('p');
+                p.innerText = "No one truly knows who's Chuck Norris' real father. No one is biologically strong enough for this. He must've conceived himself.";
+
+                div.append(commentImg, heartImg, p);
+
+                console.log(document.readyState);
+
+                //fav_container.append(div);
+                divs.push(div);
+            }
+    for (let key in divs)
+        fav_container.append(divs[key]);
+}
+
+
+function getJoke() {
+
+}
+/*
+function setCookie(name, value, options = {}) {
+
+    options = {
+        path: '/'
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+function deleteCookie(name) {
+    setCookie(name, "", {
+        'max-age': -1
+    })
+}
+var utilCookie = Cookies.get('utilCookie');
+if (typeof utilCookie === 'undefined') {
+  // performs a failsafe by falling back to a default value
+} else {
+  // uses the value of the cookie elsewhere in the app
+}
+*/
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+    var name;
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+favor();
